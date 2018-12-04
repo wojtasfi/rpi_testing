2642dacc078c2f734c1f4b740c11d1446ef3fd6c
Wojciech Figas <wojciech.figas@outlook.com>
2018-12-04 18:46:12 +0100

    add flaps

--- modules/FlapsService.py
+++ modules/FlapsService.py
@@ -10,6 +10,20 @@ class FlapsService:
         self.left_servo_pin_nr = 11
         self.initial_position = 6
         self.__setup_module()
+        self.MAX_MOVE = 12
+        self.contra_positions = {
+            1.0: 11.0,
+            2.0: 10.0,
+            3.0: 9.0,
+            4.0: 8.0,
+            5.0: 7.0,
+            6.0: 6.0,
+            7.0: 5.0,
+            8.0: 4.0,
+            9.0: 3.0,
+            10.0: 2.0,
+            11.0: 1.0
+        }
 
     def __setup_module(self):
         GPIO.setmode(GPIO.BOARD)
@@ -23,12 +37,12 @@ class FlapsService:
         self.left_servo_pin.start(self.initial_position)
 
     def move(self, flaps):
-        x = flaps["x"]
-        y = flaps["y"]
+        x = self.contra_positions.get(float(flaps["x"]))
+        y = float(flaps["y"])
 
         try:
-            self.right_servo_pin.ChangeDutyCycle(float(x))
-            self.left_servo_pin.ChangeDutyCycle(float(y))
+            self.right_servo_pin.ChangeDutyCycle(x)
+            self.left_servo_pin.ChangeDutyCycle(y)
 
         except KeyboardInterrupt:
             self.right_servo_pin.stop()
